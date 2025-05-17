bc7c6f2719ef2e87c58734465eb1edd4d077e6bb
Emanuel <limaemanuel773@gmail.com>
2025-05-16 23:39:26 -0300

    Criando a conexão com mongoBD e criando a primeria rota POST

--- emanuelAPI/server.js
+++ emanuelAPI/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(express.json());
+app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+const mongoUser = process.env.USER_NAME
+const mongoPass = process.env.USER_PASSWORD
+
+mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@clusterapi.w58r7th.mongodb.net/api_db?retryWrites=true&w=majority&appName=clusterApi`).then(() => {
+    app.listen(3000, () => {
+        console.log('Conectado ao MongoDB');
+        console.log('Server is running on port 3000');
+    })
+}).catch((err) => console.log(err));
